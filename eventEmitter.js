74613ea485d66ac28f56b36a3e75ebae2955493d
Malachi <ukomalachi@gmail.com>
2024-05-14 14:41:27 +0100

    provided custom EventEmitter

---
+++ eventEmitter.js
@@ -0,0 +1,20 @@
+// EventEmitter.js
+
+export default class EventEmitter {
+    constructor() {
+        this.eventTarget = new EventTarget();
+    }
+
+    on(eventName, listener) {
+        this.eventTarget.addEventListener(eventName, listener);
+    }
+
+    off(eventName, listener) {
+        this.eventTarget.removeEventListener(eventName, listener);
+    }
+
+    emit(eventName, data) {
+        const event = new CustomEvent(eventName, { detail: data });
+        this.eventTarget.dispatchEvent(event);
+    }
+}
